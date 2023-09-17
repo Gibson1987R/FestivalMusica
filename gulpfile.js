8fefe69fbe88027d23d58b0916b434efc6c90dae
Gibson1987R <gibsonrosales@gmail.com>
2023-09-17 12:32:33 -0400

    Terminado la funcion dev con el watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,21 @@
-function tarea(done) {
-  console.log('Anais es mi esposa hermosa')
+//todo llamar desde el package.json al gulpfile, const name = require('script')
+const { src, dest, watch } = require('gulp')
+const sass = require('gulp-sass')(require('sass')) //! Requiere instalar gulp-sass para conectar
 
+//* Los pasos a hacer dentro de la funcion:
+function css(done) {
+  src('src/scss/app.scss') // 1 identificar el archivo SASS
+    .pipe(sass()) // 2 Complilarlo
+    .pipe(dest('build/css')) // 3 Almacenarlo
+
+  done() // Callback que avisa a gulp cuando llegamos al final
+}
+
+function dev(done) {
+  watch('src/scss/app.scss', css)
   done()
 }
-//TODO para llamar esto desde node utilizamos npx gulp tarea
-exports.tarea = tarea
+exports.css = css
+exports.dev = dev
+
+//TODO para llamar esto desde node utilizamos npx gulp tarea; mientras que si usamos package.json es npm run tarea
